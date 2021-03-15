aedd397b1e882c8bf4ee5eae6726de51b016fdbd
Kristyn Little <kristynlittle@frontier.com>
2021-03-15 22:08:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,15 +17,9 @@ def long_planeteer_calls(array)# code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
-  if array.include?("cheddar")
-    true
-  elsif array.include?("gouda")
-    true
-  elsif array.include?("camembert")
-    true
-  else
-    false
-  end
+  array.include?("cheddar")
+  array.include?("gouda")
+  array.include?("camembert")
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
 end
