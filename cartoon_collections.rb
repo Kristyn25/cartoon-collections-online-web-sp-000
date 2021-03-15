3624f6a156dc649ce3824c0a966b30bb1161ebbd
Kristyn Little <kristynlittle@frontier.com>
2021-03-15 21:45:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(array)# code an argument here
   array.map{|words|words.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  if array.any{|word| word >= 4}
+    true
+  else false
 end
 
 def find_the_cheese# code an argument here
