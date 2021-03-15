42220a1f8f3dcc400e8e06d3b4e925472b84b1eb
Kristyn Little <kristynlittle@frontier.com>
2021-03-15 21:15:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each.with_index(1) do |name, index|
+    puts "#{index}.#{name}"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.map { |calls| calls.upcase }
+  end
 end
 
 def long_planeteer_calls# code an argument here
