c1e063c1b277f461e5f8ca752ca538915a970bb0
Kristyn Little <kristynlittle@frontier.com>
2021-03-15 21:57:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,6 @@ def find_the_cheese(array)# code an argument here
   else
     false
   end
-end
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
 end
